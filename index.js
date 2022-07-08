672407ecc9a52e6fc83279b145af914da78c82a0
Tony Pham <tnpham@valdosta.edu>
2022-07-08 12:09:13 -0400

    commit tree 3395b31a098d45c133571e6aaa9428487e9ca431

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(streetNumber){
+    return Math.abs(42 - streetNumber);
+}
+
+function distanceFromHqInFeet(streetNumber){
+    return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){   
+   
+    const feet = distanceTravelledInFeet(start, destination);
+
+    if (feet <= 400) return 0;
+    else if(feet >= 400 && feet <= 2000){
+
+        const fare = .02;
+        return (feet - 400) * fare;
+    }
+    else if (feet > 2000 && feet <= 2500) {
+        return 25;
+    }
+    else if (feet > 2500){
+        return 'cannot travel that far';
+    }
+
+
+}
